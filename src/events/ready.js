a90df95cc66e93d1f46615b773f131458823600d
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-13 23:16:39 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    let activities = [`EgemenDijital.com`, `EgoHost.com.tr`, `discord.gg/egemen`, `${client.user.username}`], i = 0;
+    let activities = [`MotionStudıo`, `Grave - 0.0.1`, `Yakında`, `${client.user.username}`], i = 0;
     setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Streaming, url: `https://twitch.tv/egemenxgul` }), 22000);
   }
 };
