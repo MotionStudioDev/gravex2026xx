465992b6e7c3ef962a635defbbcb12ed21b416bc
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-15 13:04:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -269,46 +269,6 @@ client.on("messageCreate", async message => {
 
 ///// küüfür son
 //caps
-if (!global.client) {
-    const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 
-    global.client = new Client({
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent
-        ]
-    });
-
-    // CapsLock kontrolü
-    global.client.capsLockAktif = true;
-
-    global.client.on("messageCreate", async message => {
-        if (!global.client.capsLockAktif) return;
-        if (!message.guild || message.author.bot) return;
-        if (!message.member || message.member.permissions.has("ManageMessages")) return;
-
-        const içerik = message.content;
-
-        // Tüm harfleri filtrele (Türkçe dahil)
-        const harfler = [...içerik].filter(c => c.match(/[a-zA-ZçÇğĞıİöÖşŞüÜ]/u));
-        if (harfler.length < 5) return;
-
-        // Büyük harf oranı
-        const büyükHarfSayısı = harfler.filter(h => h === h.toLocaleUpperCase("tr")).length;
-        const oran = büyükHarfSayısı / harfler.length;
-
-        if (oran >= 0.8) {
-            await message.delete().catch(() => {});
-
-            const embed = new EmbedBuilder()
-                .setTitle("🔇 Büyük Harf Engeli")
-                .setDescription(`**${message.author.tag}** tarafından gönderilen mesaj büyük harf içerdiği için silindi.`)
-                .setColor(0xffcc00);
-
-            const uyarı = await message.channel.send({ embeds: [embed] }).catch(() => {});
-            setTimeout(() => uyarı?.delete().catch(() => {}), 5000); // 5 saniye bekle
-        }
-    });
 
 // caps son
